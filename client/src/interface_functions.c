48a2328f43395adbdcad545ccdefa52bd1097042
anonimous0 <zagin.samovbuvc@gmail.com>
2021-02-08 13:43:10 +0200

    jknm

--- client/src/interface_functions.c
+++ client/src/interface_functions.c
@@ -143,6 +143,21 @@ void new_chat(GtkWidget *widget, gpointer data) {
     chat_name = GTK_WIDGET(gtk_builder_get_object(builder, "name"));
     chatname_entry = GTK_ENTRY(gtk_builder_get_object(builder, "name"));
 }
+void chat_menu(GtkWidget *widget, gpointer data){
+    UNUSED(widget);
+    client_t *client = (client_t *)data;
+    GtkButton  *add_user, *delete_chat;
+    GtkWidget *wind;
+
+    
+    wind = GTK_WIDGET(gtk_builder_get_object(builder, "chat_dialog"));
+    gtk_window_move(GTK_WINDOW(wind), client->m->root_x, client->m->root_y);
+    gtk_widget_show(wind);
+    add_user = GTK_BUTTON(gtk_builder_get_object(builder, "add_member"));
+    delete_chat = GTK_BUTTON(gtk_builder_get_object(builder, "delete_chat"));
+    g_signal_connect(add_user, "clicked", G_CALLBACK(open_login_page), data);
+    g_signal_connect(delete_chat, "clicked", G_CALLBACK(func_register), data);
+}
 gboolean destroy() {
     gtk_main_quit();
     return TRUE;
